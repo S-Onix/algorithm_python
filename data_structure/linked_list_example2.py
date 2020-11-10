6ba8fed50224d11531ecd152e5fb68034732d28a
S-Onix <msyoo101@hansunag.ac.kr>
2020-11-10 17:50:22 +0900

    linkedlist in node management delete method implement

--- data_structure/linked_list_example2.py
+++ data_structure/linked_list_example2.py
@@ -25,6 +25,31 @@ class NodeMgmt:
             print(node.data)
             node = node.next
 
+    # 1. head 삭제
+    # 2. 마지막 노드 삭제
+    #       마지막 노드 삭제 이후 그 이전 Node의 next 값을 None으로 변환
+    # 3. 중간 노드 삭제
+    def delete(self, data) :
+        if self.head == '':
+            print('해당 값을 가진 노드가 없습니다')
+            return
+        
+        if self.head.data == data :
+            temp = self.head
+            self.head = self.head.next
+            del temp
+        else :
+            node = self.head
+            while node.next :
+                if node.next.data == data :
+                    # 이전 노드, 이후 노드를 연결해줘야함 + 중간 노드 삭제
+                    temp = node.next
+                    node.next = node.next.next
+                    del temp
+                    return
+                else :
+                    node = node.next
+
 
 linkedlist1= NodeMgmt(0)
 linkedlist1.desc()
@@ -32,4 +57,10 @@ print()
 for index in range(1,10):
     linkedlist1.add(index)
 
+linkedlist1.desc()
+
+linkedlist1.delete(4)
+linkedlist1.delete(9)
+linkedlist1.delete(10)
+print()
 linkedlist1.desc()
