3ff5d927da94ae3862c7549272c9847d281f54a2
S-Onix <msyoo101@hansung.ac.kr>
2020-11-27 17:09:30 +0900

    tree delete method implement

--- data_structure/tree_example.py
+++ data_structure/tree_example.py
@@ -5,6 +5,8 @@
 # 2진 탐색 트리 : 이진 트리에서 추가적인 조건이 있음 >> 왼쪽 노드는 해당 노드보다 적은 값, 오른쪽 노드는 해당 노드보다 큰 값을 가지고 있어야함.
 # 장점 : 탐색 속도를 개선할 수 있다.
 # 단점 : 구현하기 쉽지 않음
+import random
+
 
 #노드 구현
 class Node :
@@ -80,6 +82,7 @@ class NodeMgt :
             else :
                 self.parent_node.right = None
             del self.current_node
+        
         # 삭제할 노드의 Child Node가 하나일때
         # left에 존재할 경우
         if self.current_node.left != None and self.current_node.right == None :
@@ -96,14 +99,67 @@ class NodeMgt :
             else :
                 self.parent_node.right = self.current_node.right
             del self.current_node
-
-        # 삭제할 노드의 Child Node가 두개일때
-
-
         
+        # 삭제할 노드의 Child Node가 두개일때
+        # 삭제할 노드가 parent의 왼쪽에 있을 경우
+        #   삭제할 노드의 오른쪽 자식 중 가장 작은 값을 삭제할 위치로 올리는 경우
+        #       가장 작은 노드의 자식 노드가 존재할 경우
+        #       가장 작은 노드의 자식 노드가 존재하지 않을 경우
+        #   삭제할 노드의 왼쪽 자식 중 가장 큰 값을 삭제할 위치로 올리는 경우
+        #       가장 큰 노드의 자식 노드가 존재할 경우
+        #       가장 큰 노드의 자식 노드가 존재하지 않을 경우
+        # 삭제할 노드가 parent의 오른쪽에 있을 경우
+        if self.current_node.left != None and self.current_node.right != None :
+            # 삭제할 노드의 값(위치)이 부모의 노드보다 작을때 >> 왼쪽에 존재할 경우
+            if value < self.parent_node.value :
+                # 삭제할 노드는 current_node 이다.
+                # 변경될 노드와 변결될 노드의 부모의 초기값은 삭제될 노드의 오른쪽 노드이다.
+                self.change_node_parent = self.current_node.right
+                self.change_node = self.current_node.right
+                # 최소 값을 가지는 노드를 찾기 위한 순회 과정
+                while self.change_node.left :
+                    self.change_node_parent = self.change_node
+                    self.change_node = self.change_node.left
+                # 최소 값을 가지는 노드가 자식 노드를 가지고 있는 경우 (오른쪽 노드만 가지고 있음)
+                if self.change_node.right :
+                    # 변경될 노드의 부모 노드와 변경될 노드의 자식 노드를 연결해줘야한다.
+                    self.change_node_parent.left = self.change_node.right
+                else : 
+                    # 변경될 부모 노드의 왼쪽을 끊어준다.(순환구조(Cycle) 형성이 되면 안되기 때문에)
+                    self.change_node_parent.left = None
+                # 삭제될 노드의 왼쪽을 변경될 노드로 연결해준다.
+                self.parent_node.left = self.change_node
+                # 삭제될 노드의 오른쪽 노드와 왼쪽노드를 변경될 노드의 왼쪽 노드와 오른쪽 노드로 변경해준다.
+                self.change_node.left = self.current_node.left
+                self.change_node.right = self.current_node.right
+                
+            else :
+                self.change_node_parent = self.current_node.right
+                self.change_node = self.current_node.right
+                while self.change_node.left :
+                    self.change_node_parent = self.change_node
+                    self.change_node = self.change_node.left
+                if self.change_node.right :
+                    self.change_node_parent.left = self.change_node.right
+                else :
+                    self.change_node_parent.left = None
+                self.parent_node.right = self.change_node
+                self.change_node.left = self.current_node.left
+                self.change_node.right = self.current_node.right
+            
+            # 노드를 삭제한다.
+            del self.current_node
+        return True
 
 head = Node(1)
 BST = NodeMgt(head)
 BST.insert(3)
 print(BST.search(3))
 print(BST.search(2))
+
+# 1-1000 숫자 중 임의로 100개 추출, 이진 탐색 트리에서 입력, 검색, 삭제
+bst_nums = set()
+while len(bst_nums) != 100 :
+    bst_nums.add(random.randint(0,999))
+
+print(bst_nums)
